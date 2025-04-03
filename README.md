cc463f1b0c019aaa577c8904bdceffc1ae75be59
Muhammad Ramadhan Fathoni <mramadhanfathoni@gmail.com>
2025-04-03 18:58:15 +0700

    inital commit

---
+++ README.md
@@ -0,0 +1,5 @@
+nama : Muhammad Ramadhan Fathoni
+
+nim : 607062300055
+
+ASSESMENT01 MOBPRO1
---
+++ app/src/main/java/com/toni/goldcalc/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.toni.goldcalc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.toni.goldcalc.ui.theme.GoldcalcTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GoldcalcTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GoldcalcTheme {
+        Greeting("Android")
+    }
+}
