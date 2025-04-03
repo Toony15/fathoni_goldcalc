0d16881b3a7cbaf4b60ed088c8dbc4f94b620a06
Muhammad Ramadhan Fathoni <mramadhanfathoni@gmail.com>
2025-04-03 19:44:05 +0700

    Top App Bar Progressin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.toni.goldcalc"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.toni.goldcalc"
--- app/src/main/java/com/toni/goldcalc/MainActivity.kt
+++ app/src/main/java/com/toni/goldcalc/MainActivity.kt
@@ -6,10 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.toni.goldcalc.ui.theme.GoldcalcTheme
 
@@ -19,15 +26,37 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GoldcalcTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+               MainScreen()
             }
         }
     }
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar =  {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor =  Color(0xFFFFD700),
+                    titleContentColor = androidx.compose.ui.graphics.Color.Black,
+                )
+            )
+        }
+    ) { innerPadding ->
+        ScreenContent(Modifier.padding(innerPadding))
+
+    }
+}
+@Composable
+fun  ScreenContent(modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello Android!",
+        modifier =  modifier
+    )
 }
 
 @Composable
@@ -42,6 +71,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GoldcalcTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
